d9547dcc23b3964bf13089084216f6131558e385
taqiabdulaziz <svx.annexiv@gmail.com>
2018-09-26 02:18:44 +0700

    exercise 10 week 2

---
+++ week2/exercise10.js
@@ -0,0 +1,18 @@
+/*eslint-disable*/
+
+function bandingkanAngka(angka1, angka2) {
+  if (angka1 > angka2) {
+    return false;
+  } else if (angka1 == angka2) {
+    return -1;
+  } else {
+    return true;
+  }
+}
+
+// TEST CASES
+console.log(bandingkanAngka(5, 8)); // true
+console.log(bandingkanAngka(5, 3)); // false
+console.log(bandingkanAngka(4, 4)); // -1
+console.log(bandingkanAngka(3, 3)); // -1
+console.log(bandingkanAngka(17, 2)); // false
