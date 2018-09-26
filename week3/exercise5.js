fdcf929210e972cab4b78cd24352cf09a67f7f87
taqiabdulaziz <svx.annexiv@gmail.com>
2018-09-27 01:07:48 +0700

    exercise 5 week 3

---
+++ week3/exercise5.js
@@ -0,0 +1,22 @@
+/*eslint-disable*/
+
+function palindrome(kata) {
+  var reversedWord = "";
+  var boolean;
+  for (let index = kata.length; index >= 0; index--) {
+    reversedWord += kata.charAt(index);
+  }
+
+  if (reversedWord == kata) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// TEST CASES
+console.log(palindrome("katak")); // true
+console.log(palindrome("blanket")); // false
+console.log(palindrome("civic")); // true
+console.log(palindrome("kasur rusak")); // true
+console.log(palindrome("mister")); // false
