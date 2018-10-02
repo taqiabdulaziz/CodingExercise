9126aecd711b1fb8c24db039d9b6781e8035db90
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-03 00:23:31 +0700

    exercise 6 week 4

---
+++ week4/exercise6.js
@@ -0,0 +1,46 @@
+function hitungHuruf(kata) {
+  var arrKata = kata.split(" ");
+  var counter = 0;
+  var counterTemp = 0;
+  var wordArr = [];
+  var wordArrTemp = 0;
+  var word = "";
+  var wordTemp = "";
+
+  for (let i = 0; i < arrKata.length; i++) {
+    counterTemp = 0;
+    wordArrTemp = wordArr;
+    wordArrCounter = wordArr.length;
+    wordTemp = word;
+    wordArr = [];
+
+    for (let j = 0; j < arrKata[i].length; j++) {
+      for (let k = 0; k < arrKata[i].length; k++) {
+        if (
+          arrKata[i].charAt(k) == arrKata[i].charAt(j) &&
+          j != k &&
+          wordArr.indexOf(arrKata[i].charAt(k)) == -1
+        ) {
+          counterTemp += 1;
+
+          if (wordArr.indexOf(arrKata[i].charAt(k)) == -1) {
+            wordArr.push(arrKata[i].charAt(j));
+          }
+          if (counter < counterTemp) {
+            counter = counterTemp;
+            word = arrKata[i];
+          }
+        }
+      }
+    }
+  }
+
+  return word;
+}
+
+// TEST CASES
+console.log(hitungHuruf("Today, is the greatest day ever")); // greatest
+console.log(hitungHuruf("I am a passionate developer")); // passionate
+console.log(hitungHuruf("aku adalah anak gembala")); // adalah
+console.log(hitungHuruf("rajin pangkal kaya")); // pangkal
+console.log(hitungHuruf("mengayuh perahu di danau")); // danau
