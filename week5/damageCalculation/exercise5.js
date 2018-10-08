965f81fc673493d422c54c3999a8400f8dbe9318
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-08 22:42:13 +0700

    Add exercise 5 week 5

---
+++ week5/damageCalculation/exercise5.js
@@ -0,0 +1,15 @@
+/*eslint-disable*/
+function attack(damage) {
+  return damage - 2;
+}
+
+function damageCalculation(numberOfAttacks, damagePerAttack) {
+  return attack(damagePerAttack) * numberOfAttacks;
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
