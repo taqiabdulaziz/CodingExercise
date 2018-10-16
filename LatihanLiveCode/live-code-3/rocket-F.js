bec0b45739b04f1af7a72b60ed6bc7db1a124de9
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-17 01:48:50 +0700

    add exercise completion

---
+++ LatihanLiveCode/live-code-3/rocket-F.js
@@ -0,0 +1,40 @@
+/*eslint-disable*/
+/*
+------------------------
+Count Letter Recursively
+------------------------
+PROBLEM
+========
+Tersedia 2 input: suatu kata dan suatu huruf
+Carilah berapa kali huruf muncul di dalam kata.
+Gunakan rekursif untuk menyelesaikan soal ini.
+Contoh
+1) sentence = 'the quick brown fox', letter = 'o'    -->     2
+RULES
+=====
+- Wajib menggunakan metode rekursif
+- Dilarang menambahkan parameter baru
+- Dilarang membuat variable di luar function countLetterRecursive
+- Dilarang menggunakan looping (while, for, do-while, dan lain-lain)
+*/
+
+function countLetterRecursive(sentence, letter) {
+  if (sentence.charAt(sentence.length - 1) == "") {
+    return 0;
+  } else if (sentence.charAt(sentence.length - 1) == letter) {
+    return (
+      1 +
+      countLetterRecursive(sentence.substring(0, sentence.length - 1), letter)
+    );
+  } else {
+    return countLetterRecursive(sentence.substring(0, sentence.length - 1), letter);
+  }
+
+  
+}
+
+console.log(countLetterRecursive("12104123", "1")); // 3
+console.log(countLetterRecursive("the quick brown fox", "o")); // 2
+console.log(countLetterRecursive("lorem ipsum", "a")); // 0
+console.log(countLetterRecursive("hahaha", "h")); // 3
+console.log(countLetterRecursive("", "x")); // 0
