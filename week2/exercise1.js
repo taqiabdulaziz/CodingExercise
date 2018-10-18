0cb062b3bf4fbba41115c5c300bf191ec1253297
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-18 23:34:08 +0700

    complete algorithm for exercise 1 week 2

--- week2/exercise1.js
+++ week2/exercise1.js
@@ -1,3 +1,4 @@
+
 1. 2ND LAW NEWTON---------------------------------------
 
 INITIALIZE massa benda TO 600
@@ -8,6 +9,12 @@ SAVE PREVIOUS COMPUTATION AS resultan
 
 2. LEAP YEAR----------------------------------------------
 
+ALGORITHM:
+STEP 1: get year number first
+STEP 2: if year number divided by 4 equal to 0, then proceed to step 3
+STEP 3: if year number divided by 100 is not equal to 0, then proceed to step 4
+STEP 4: if year number divided by 400 equal to 0, then year number is leap year 
+
 INITIALIZE year
 IF COMPUTE year MOD 4 EQUAL TO 0
     AND year MOD 100 NOT EQUAL TO 0
@@ -15,6 +22,12 @@ IF COMPUTE year MOD 4 EQUAL TO 0
     THEN PRINT "year is leap"
 
 3. LAUNDRY DAY-------------------------------------------
+
+ALGORITHM:
+STEP 1: get cucian kotor quantity, and cucian di mesin quantity is must be zero
+STEP 2: if cucian kotor quantity is zero, then nothing to do :), other than that then proceed to step 3
+STEP 3: move cucian kotor quantity to cucian di mesin one-by-one until cucian kotor quantity reach zero, and then proceed to step 4
+STEP 4: run mesin cuci
 
 INITIALIZE cucian kotor TO 20
 INITIALIZE cucian di mesin TO 0
@@ -32,6 +45,12 @@ WHILE
 GENERATE mesin cuci
 
 4. PERIKSA KUKU-----------------------------------------
+
+ALGORITHM:
+STEP 1: get numbers of siswa siswi, if numbers of siswa siswi is 0 then nothing to do, other than that proceed to step 2
+STEP 2: get length of 'kuku' every siswa siswi, then proceed to step 3
+STEP 3: check length of 'kuku' every siswa siswi respectively
+STEp 4: if from step 3 returned value "panjang" then run 'hukuman', else if from step 3 return value "pendek" then run 'pujian'
 
 INITIALIZE siswa siswi TO 40
 WHILE siswa siswi VALUE IS LESS THAN OR EQUAL TO 40
